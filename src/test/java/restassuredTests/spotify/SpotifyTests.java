e3c0fb3fc6495fdb3444561636631bb297cb780c
Rafael Elias <rafaeleliasw@gmail.com>
2019-11-14 15:52:29 -0300

    removed Spotify variables

--- src/test/java/restassuredTests/spotify/SpotifyTests.java
+++ src/test/java/restassuredTests/spotify/SpotifyTests.java
@@ -22,15 +22,15 @@ import static restassuredTests.TestUtils.getSpotifyAccessToken;
 
 public class SpotifyTests {
 
-    public static final String clientId = "6aa4abdbfa974048883221cf82d67cc8";
-    public static final String redirect_uri = "http://mysite.com/callback/";
-    public static final String scope = "user-read-private user-read-email";
-    public static final String response_type = "token";
+    public static final String clientId = "";
+    public static final String redirect_uri = "";
+    public static final String scope = "";
+    public static final String response_type = "";
     public static final int  state = 123;
-    public static final String userId = "xpyan8kpzi8nfu9nci6gfc1nq";
+    public static final String userId = "";
     public static String bearer = "";
-    public static final String username = "codingclubrosario2019@gmail.com";
-    public static final String password = "C@ding.club.2k19";
+    public static final String username = "";
+    public static final String password = "";
 
     @BeforeClass
     public static void authenticationSpotify() throws InterruptedException, UnsupportedEncodingException {
