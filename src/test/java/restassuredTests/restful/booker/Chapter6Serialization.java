18e8de1f50ee990b55f950ef790bbae482aed1e5
Rafael Elias <rafaeleliasw@gmail.com>
2019-11-25 15:44:56 -0300

    Added Serialization Test

--- src/test/java/restassuredTests/restful/booker/Chapter6Serialization.java
+++ src/test/java/restassuredTests/restful/booker/Chapter6Serialization.java
@@ -1,15 +1,31 @@
 package restassuredTests.restful.booker;
 
 import dataentities.Booking;
+import dataentities.Bookingdates;
 import dataentities.Location;
+import io.restassured.http.ContentType;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import restassuredTests.TestUtils;
+
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 public class Chapter6Serialization {
+
+    public static final String username = "";
+    public static final String password = "";
+    public static String token = "";
+
+    @BeforeClass
+    public static void beforeTests() throws IOException {
+        //Get Encoded Token in Base64
+        token = TestUtils.getEncodedToken(username, password);
+    }
+
 
     @DataProvider(name = "zipCodesAndPlaces")
     public static Object[][] zipCodesAndPlaces() {
@@ -50,5 +66,32 @@ public class Chapter6Serialization {
         Assert.assertEquals("Susan", booking.getFirstname());
         Assert.assertEquals (574, booking.getTotalprice());
 
+    }
+
+    @Test
+    public void sendLvZipCode1050_checkStatusCode_expect200() {
+
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2020-02-23");
+        bookingdates.setCheckout("2020-02-28");
+
+        Booking booking = new Booking();
+        booking.setFirstname("Rafa");
+        booking.setLastname("Elias");
+        booking.setDepositpaid(true);
+        booking.setTotalprice(200);
+        booking.setBookingdates(bookingdates);
+        booking.setAdditionalneeds("Testing serialization");
+
+
+        given().
+                header("Authorization", "Basic "+token).
+                header("Content-Type", "application/json").
+                body(booking).
+                log().body().
+                when().
+                put("https://restful-booker.herokuapp.com/booking/1").
+                then().
+                statusCode(200);
     }
 }
